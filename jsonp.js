db7a3948b1462244f7bec5183199d59ee6374606
Bradley Lancaster <bradleylancaster101@gmail.com>
2017-08-29 00:18:17 +1000

    Initial Commit
    Json

---
+++ jsonp.js
@@ -0,0 +1,15 @@
+showFlights({
+  "flights": [
+	  {
+		"location" : "Dublin",
+		"desc" : "return from",
+        "price" : "999"
+	  },
+	  {
+		"location" : "Carabbean",
+		"desc" : "!0 Night Cruise",
+         "price" : "1299"
+      }
+	]
+});
+
